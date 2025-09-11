bb5003e09a1237d803da77a0a6115671ddc0e18a
elidaniels <eli.daniels@ardata.fr>
2025-09-11 17:58:39 +0200

    feat: make shared border calculating non-blocking

--- R/app-onstart.R
+++ R/app-onstart.R
@@ -5,12 +5,15 @@
 #' adds a resource path for the app's static files.
 #' @importFrom gdtools register_liberationsans
 #' @importFrom shiny addResourcePath
+#' @importFrom mirai daemons
 #' @export
 onstart <- function() {
   options(shiny.maxRequestSize = 30 * 1024^3) # for uploading large files with shiny::fileInput
   register_liberationsans()
   register_fontawesome()
   # cirad_gg_theme_set()
+  mirai::daemons(2)
+  onStop(function() mirai::daemons(0))
   addResourcePath("riskintro", system.file(package = "riskintroapp", "www"))
   options(shiny.useragg = TRUE)
   invisible()
--- R/module-tab-border-risk.R
+++ R/module-tab-border-risk.R
@@ -9,10 +9,11 @@ borderRiskUI <- function(id) {
       title = "Border risk",
       uiOutput(ns("config_is_valid")),
       tags$br(),
-      actionButton(
-        inputId = ns("calculate_border_lengths"),
+      bslib::input_task_button(
+        id = ns("calc"),
         label = "Calculate shared borders",
-        icon = icon("map")
+        icon = icon("map"),
+        label_busy = "Calculating..."
       ),
       tags$br(),
       actionButton(
@@ -38,8 +39,12 @@ borderRiskUI <- function(id) {
 #' @importFrom sf st_drop_geometry
 #' @importFrom leaflet renderLeaflet leafletProxy addPolygons
 #' @importFrom reactable reactable renderReactable
-#' @importFrom shiny moduleServer observeEvent reactive req
+#' @importFrom shiny moduleServer observeEvent reactive req updateActionButton
 #' @importFrom riskintroanalysis calc_border_lengths calc_border_risk
+#' @importFrom bslib bind_task_button
+#' @importFrom mirai mirai
+#' @importFrom shiny ExtendedTask
+#' @importFrom shinyWidgets show_toast
 borderRiskServer <- function(id, input_data, epi_units, emission_scores) {
   moduleServer(
     id,
@@ -55,76 +60,6 @@ borderRiskServer <- function(id, input_data, epi_units, emission_scores) {
         inverse = FALSE
       ))
 
-      # configIsValid ----
-      configIsValid <- reactive(label = paste0("configIsValid-", id), {
-        if (!isTruthy(epi_units())) {
-          status <- build_config_status(
-            value = FALSE,
-            msg = "Epidemiological units dataset must be imported."
-          )
-          return(status)
-        }
-
-        if (!isTruthy(emission_scores())) {
-          status <- build_config_status(
-            value = FALSE,
-            msg = "Emission risk scores must be calculated."
-          )
-          return(status)
-        }
-
-        if (!isTruthy(input_data())) {
-          status <- build_config_status(
-            value = FALSE,
-            msg = "Shared borders has not been calculated, click the button above to start."
-          )
-          return(status)
-        }
-
-        if (is_error(input_data()$error)) {
-          status <- build_config_status(
-            value = FALSE,
-            msg = "There was an error calculating shared borders:",
-            error = input_data()$error
-          )
-          return(status)
-        }
-
-        if (!isTruthy(riskScores())) {
-          status <- build_config_status(
-            value = FALSE,
-            msg = "Border risk has not been calculated."
-          )
-          return(status)
-        }
-
-        if (is_error(riskScores()$error)) {
-          status <- build_config_status(
-            value = FALSE,
-            msg = "Error while calculating border risk:",
-            error = riskScores()$error
-          )
-          return(status)
-        }
-
-        if (!isTruthy(rescaledScores())) {
-          status <- build_config_status(
-            value = FALSE,
-            msg = "Risk scores cannot be rescaled."
-          )
-          return(status)
-        }
-
-        build_config_status(
-          value = TRUE,
-          msg = "Configuration is valid."
-        )
-      })
-
-      output$config_is_valid <- renderUI({
-        report_config_status(configIsValid())
-      })
-
       # Initialize maps ----
       baseMap <- reactive({ basemap() })
       output$map <- renderLeaflet({
@@ -133,20 +68,46 @@ borderRiskServer <- function(id, input_data, epi_units, emission_scores) {
       })
       outputOptions(output, "map", suspendWhenHidden = FALSE)
 
-      # borders ----
-      observeEvent(input$calculate_border_lengths, {
-        safely_calc_borders <- safely(calc_border_lengths)
-        result <- safely_calc_borders(epi_units = epi_units())
-        input_data(result)
+      # Setup async "function" ----
+      async_calc_borders <- ExtendedTask$new(
+        function(x) {
+          mirai::mirai(.expr = {
+            safely_calc_borders <- purrr::safely(riskintroanalysis::calc_border_lengths)
+            result <- safely_calc_borders(epi_units = x)
+            result
+          },
+          .args = list(x = x)
+          )
+          }
+      ) |> bind_task_button("calc")
+
+
+      # Button click starts process
+      observeEvent(input$calc, {
+        req(epi_units())
+        async_calc_borders$invoke(x = epi_units())
+      })
+      # Reactive monitors process
+      new_input_data <- reactive({
+        async_calc_borders$result()
+      })
+      # Once new value arrives, replace input_data()
+      observeEvent(new_input_data(), {
+        show_toast(
+          "Border risk has finished calculating.",
+          type = "info",
+          timer = 5000
+          )
+        input_data(new_input_data())
       })
 
       # calc_* ----
       observe({
-        req(input_data(), !is_error(input_data()$error))
+        req(input_data())
+        req(!is_error(input_data()$error))
         req(epi_units(), emission_scores())
-
-        safely_calc_risk <- safely(calc_border_risk)
-        result <- safely_calc_risk(
+        safely_calc <- safely(calc_border_risk)
+        result <- safely_calc(
           epi_units = epi_units(),
           shared_borders = input_data()$result,
           emission_risk = emission_scores()
@@ -181,7 +142,67 @@ borderRiskServer <- function(id, input_data, epi_units, emission_scores) {
         )
       })
 
-      # map ----
+      # configIsValid ----
+      configIsValid <- reactive(label = paste0("configIsValid-", id), {
+        if (!isTruthy(epi_units())) {
+          status <- build_config_status(
+            value = FALSE,
+            msg = "Epidemiological units dataset must be imported."
+          )
+          return(status)
+        }
+        if (!isTruthy(emission_scores())) {
+          status <- build_config_status(
+            value = FALSE,
+            msg = "Emission risk scores must be calculated."
+          )
+          return(status)
+        }
+        if (!isTruthy(input_data())) {
+          status <- build_config_status(
+            value = FALSE,
+            msg = "Shared borders has not been calculated, click the button above to start."
+          )
+          return(status)
+        }
+        if (is_error(input_data()$error)) {
+          status <- build_config_status(
+            value = FALSE,
+            msg = "There was an error calculating shared borders:",
+            error = input_data()$error
+          )
+          return(status)
+        }
+        if (!isTruthy(riskScores())) {
+          status <- build_config_status(
+            value = FALSE,
+            msg = "Border risk has not been calculated."
+          )
+          return(status)
+        }
+        if (is_error(riskScores()$error)) {
+          status <- build_config_status(
+            value = FALSE,
+            msg = "Error while calculating border risk:",
+            error = riskScores()$error
+          )
+          return(status)
+        }
+        if (!isTruthy(rescaledScores())) {
+          status <- build_config_status(
+            value = FALSE,
+            msg = "Risk scores cannot be rescaled."
+          )
+          return(status)
+        }
+        build_config_status(
+          value = TRUE,
+          msg = "Configuration is valid."
+        )
+      })
+      output$config_is_valid <- renderUI({
+        report_config_status(configIsValid())
+      })
 
       # table ----
       output$table <- renderReactable({
@@ -196,6 +217,7 @@ borderRiskServer <- function(id, input_data, epi_units, emission_scores) {
         )
       })
 
+      # map ----
       # returnData ----
       returnData <- reactiveVal(NULL)
       observe({
