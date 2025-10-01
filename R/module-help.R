0367d125fdf319440fde2a47ed18bae62ef99dd0
elidaniels <eli.daniels@ardata.fr>
2025-10-01 09:51:15 +0200

    fix: help links now work for additional and emission scores

--- R/module-help.R
+++ R/module-help.R
@@ -16,12 +16,12 @@ helpServer <- function(id = "help", selected_tab) {
         base_url <- "https://astre.gitlab.cirad.fr/riskintro-app/riskintroanalysis"
         help_urls <- list(
           "study_settings" = paste0(base_url, "/articles/introduction-risk.html"),
-          "emission_risk" = paste0(base_url, "/articles/emission-risk-analysis.html"),
+          "emission_risk" = paste0(base_url, "/articles/emission-scores.html"),
           "nav_border_risk" = paste0(base_url, "/articles/border-risk-analysis.html"),
           "nav_animal_movement_risk" = paste0(base_url, "/articles/animal-mobility-analysis.html"),
           "nav_entry_point_risk" = paste0(base_url, "/articles/entry-points-analysis.html"),
           "nav_road_access_risk" = paste0(base_url, "/articles/road-access-analysis.html"),
-          "nav_misc_risk" = paste0(base_url, "/articles/miscellaneous-risk-analysis.html"),
+          "nav_misc_risk" = paste0(base_url, "/articles/additional-risks.html"),
           "about" = base_url
         )
 
